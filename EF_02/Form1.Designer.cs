0d3c17798099e3e0a9f8df9b0eb1788bcdd8cc28
Goktug YILDIZ <102765970+GktgYildiz@users.noreply.github.com>
2023-02-06 21:57:17 +0300

    Add files via upload

---
+++ EF_02/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace EF_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnGetir = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnekle = new System.Windows.Forms.Button();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Tan;
+            this.groupBox1.Controls.Add(this.btnGetir);
+            this.groupBox1.Controls.Add(this.btnSil);
+            this.groupBox1.Controls.Add(this.btnGuncelle);
+            this.groupBox1.Controls.Add(this.btnekle);
+            this.groupBox1.Controls.Add(this.txtTel);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtSoyad);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtAd);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Constantia", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(302, 341);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kisi Bilgisi";
+            // 
+            // btnGetir
+            // 
+            this.btnGetir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnGetir.Font = new System.Drawing.Font("Constantia", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGetir.Location = new System.Drawing.Point(46, 282);
+            this.btnGetir.Name = "btnGetir";
+            this.btnGetir.Size = new System.Drawing.Size(182, 30);
+            this.btnGetir.TabIndex = 9;
+            this.btnGetir.Text = "Getir";
+            this.btnGetir.UseVisualStyleBackColor = false;
+            this.btnGetir.Click += new System.EventHandler(this.btnGetir_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.BackColor = System.Drawing.Color.Red;
+            this.btnSil.Font = new System.Drawing.Font("Constantia", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSil.Location = new System.Drawing.Point(46, 230);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(182, 30);
+            this.btnSil.TabIndex = 8;
+            this.btnSil.Text = "Sil!";
+            this.btnSil.UseVisualStyleBackColor = false;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnGuncelle.Font = new System.Drawing.Font("Constantia", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuncelle.Location = new System.Drawing.Point(147, 175);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(81, 30);
+            this.btnGuncelle.TabIndex = 7;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = false;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnekle
+            // 
+            this.btnekle.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnekle.Font = new System.Drawing.Font("Constantia", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnekle.Location = new System.Drawing.Point(46, 175);
+            this.btnekle.Name = "btnekle";
+            this.btnekle.Size = new System.Drawing.Size(81, 30);
+            this.btnekle.TabIndex = 6;
+            this.btnekle.Text = "Ekle";
+            this.btnekle.UseVisualStyleBackColor = false;
+            this.btnekle.Click += new System.EventHandler(this.btnekle_Click);
+            // 
+            // txtTel
+            // 
+            this.txtTel.Font = new System.Drawing.Font("Constantia", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTel.Location = new System.Drawing.Point(121, 123);
+            this.txtTel.Multiline = true;
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(147, 20);
+            this.txtTel.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Constantia", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "TEL";
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Font = new System.Drawing.Font("Constantia", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSoyad.Location = new System.Drawing.Point(122, 79);
+            this.txtSoyad.Multiline = true;
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(147, 20);
+            this.txtSoyad.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Constantia", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Soyad";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Font = new System.Drawing.Font("Constantia", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAd.Location = new System.Drawing.Point(122, 39);
+            this.txtAd.Multiline = true;
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(147, 20);
+            this.txtAd.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Constantia", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(333, 29);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(455, 341);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            this.dataGridView1.CellLeave += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 397);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnGetir;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnekle;
+        private System.Windows.Forms.TextBox txtTel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ EF_02/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EF_02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        TelRehberiDBEntities db = new TelRehberiDBEntities();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void KisileriGetir()
+        {
+            try
+            {
+                dataGridView1.DataSource = db.Kisilers.Select(k => new
+                {
+                  
+                    KisiId = k.KisiID,
+                    KisiAdi = k.KisiAdi,
+                    KisiSoyadi = k.KisiSoyadi,
+                    TelNo = k.TelNo
+                }).ToList();
+                dataGridView1.Columns[0].Visible = false;
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Kişiler gösterilemiyor");
+            }
+           
+        }
+        private void btnGetir_Click(object sender, EventArgs e)
+        {
+            //dataGridView1.DataSource = db.Kisilers.Select(k => new { k.KisiAdi, k.KisiSoyadi, k.TelNo });
+
+            //---------bu şekilde yazdığımzıda kolonlara istediğimiz ismide vermiş oluyoruz. aşşağıda kolan isimler KisiAdi,soaydi ... oto geldi//
+            KisileriGetir();
+
+        }
+
+        private void KisileriEkle()
+        {
+            try
+            {
+                Kisiler k = new Kisiler();
+               
+                k.KisiAdi = txtAd.Text;
+                k.KisiSoyadi = txtSoyad.Text;
+                k.TelNo = txtTel.Text;
+               
+                    db.Kisilers.Add(k); //veriyi modele aktardık ama daha database'e yollamadık
+                    db.SaveChanges();//burada artık veriyi veritabanına gönderiyoruz
+                
+               
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("eklenemedi");
+            }
+            
+        }
+        private void btnekle_Click(object sender, EventArgs e)
+        {
+            KisileriEkle();
+            KisileriGetir();
+            Temizle();
+        }
+
+        private void Temizle()
+        {
+            foreach (Control item in groupBox1.Controls)
+            {
+                if (item is TextBox)
+                {
+                    TextBox txt = (TextBox)item;
+                    txt.Clear();
+                }
+            }
+        }
+        Kisiler guncellenecek;
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count>0)
+            {
+                int id = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[0].Value);
+
+                guncellenecek = db.Kisilers.Find(id);
+
+                txtAd.Text = guncellenecek.KisiAdi;
+                txtSoyad.Text = guncellenecek.KisiSoyadi;
+                txtTel.Text = guncellenecek.TelNo;
+            }
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            guncellenecek.KisiAdi = txtAd.Text;
+            guncellenecek.KisiSoyadi = txtSoyad.Text;
+            guncellenecek.TelNo = txtTel.Text;
+
+            db.SaveChanges();
+            KisileriGetir();
+
+        }
+
+        private void dataGridView1_CellLeave(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[0].Value);
+            db.Kisilers.Remove(db.Kisilers.Find(id));
+            db.SaveChanges();
+            KisileriGetir();
+        }
+    }
+}
